efcc8b212e4545801c98b103123b50e273c93c6b
zhangtaoshan <zhangtaoshan@sensetime.com>
2026-03-30 14:18:53 +0800

    add dockerfile

---
+++ setup_ascend.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+package_data = {"lightllm": ["common/all_kernel_configs/*/*.json", "common/triton_utils/*/*/*/*/*.json"]}
+setup(
+    name="lightllm",
+    version="1.1.0",
+    packages=find_packages(exclude=("build", "include", "test", "dist", "docs", "benchmarks", "lightllm.egg-info")),
+    author="model toolchain",
+    author_email="",
+    description="lightllm for inference LLM",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    python_requires=">=3.9.16",
+    install_requires=[
+        "pyzmq",
+        "uvloop",
+        "transformers",
+        "einops",
+        "packaging",
+        "rpyc",
+        "ninja",
+        "safetensors",
+        "orjson",
+    ],
+    package_data=package_data,
+)
